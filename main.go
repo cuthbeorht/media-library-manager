92ec4f38b050b51cb3c3e6d05ca527bc3f670e07
David Sciacchettano <david.sciacchettano@gmail.com>
2024-08-10 20:22:21 -0400

    Fix stuff

--- main.go
+++ main.go
@@ -3,18 +3,19 @@ package main
 import (
 	"fmt"
 
-	"github.com/cuthbeorht/media-library-manager/internal/auth"
 	"github.com/cuthbeorht/media-library-manager/internal/config"
-	"github.com/cuthbeorht/media-library-manager/internal/files"
+	"github.com/cuthbeorht/media-library-manager/internal/dropbox/auth"
+	"github.com/cuthbeorht/media-library-manager/internal/dropbox/files"
 	"github.com/cuthbeorht/media-library-manager/internal/walker"
 )
 
 func main() {
 	fmt.Println("Initializing Media Library Manager")
 
-	token := auth.Auth()
-	appConfig := config.Config(token.AccessToken)
-	filesClient := files.Client(appConfig)
+	appConfig := config.NewApplicationConfig()
+	token := auth.Auth(appConfig)
+	dropboxConfig := config.DropboxConfig(token.AccessToken)
+	filesClient := files.Client(dropboxConfig)
 
 	walker.WalkMediaDir(filesClient, "/New_Music/music/Mass Effect 2 OST")
 

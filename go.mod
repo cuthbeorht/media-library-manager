b6be3a58e40a7ffe89117c7cfa10047d071f47c5
David <cuthbeorht@users.noreply.github.com>
2024-08-15 19:16:20 -0400

    Persist music data (#2)
    * Fix stuff
    
    * Create a table
    
    * Insert audio files works
    
    * Complete simple persistence of audio files

--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ go 1.21.5
 
 require golang.org/x/oauth2 v0.21.0
 
-require github.com/dropbox/dropbox-sdk-go-unofficial/v6 v6.0.5 // indirect
+require (
+	github.com/dropbox/dropbox-sdk-go-unofficial/v6 v6.0.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+)
--- main.go
+++ main.go
@@ -2,20 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/cuthbeorht/media-library-manager/internal/auth"
 	"github.com/cuthbeorht/media-library-manager/internal/config"
-	"github.com/cuthbeorht/media-library-manager/internal/files"
+	database "github.com/cuthbeorht/media-library-manager/internal/database/sql"
+	"github.com/cuthbeorht/media-library-manager/internal/dropbox/auth"
+	"github.com/cuthbeorht/media-library-manager/internal/dropbox/files"
 	"github.com/cuthbeorht/media-library-manager/internal/walker"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Initializing Media Library Manager")
 
-	token := auth.Auth()
-	appConfig := config.Config(token.AccessToken)
-	filesClient := files.Client(appConfig)
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Could not find environment variavles")
+	}
 
-	walker.WalkMediaDir(filesClient, "/New_Music/music/Mass Effect 2 OST")
+	appConfig := config.NewApplicationConfig()
+
+	dbConn := database.Connect(appConfig.DatabasePath)
+	defer dbConn.Close()
+	database.CreateTables(dbConn)
+
+	token := auth.Auth(appConfig)
+	dropboxConfig := config.DropboxConfig(token.AccessToken)
+	filesClient := files.Client(dropboxConfig)
+
+	walker.WalkMediaDir(dbConn, filesClient, "/New_Music/music")
 
 }
